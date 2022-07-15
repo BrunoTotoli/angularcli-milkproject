e8483ec31a1a095913ccf21f37f3e2612722d23c
Bruno Totoli <santosbruno101062@gmail.com>
2022-07-15 00:40:26 -0300

    HttpClient to find data

--- src/app/milks/milks/milks.component.ts
+++ src/app/milks/milks/milks.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Milk} from "../model/milk";
 import {MilksService} from "../services/milks.service";
+import {Observable} from "rxjs";
 
 
 @Component({
@@ -8,22 +9,18 @@ import {MilksService} from "../services/milks.service";
   templateUrl: './milks.component.html',
   styleUrls: ['./milks.component.scss']
 })
-export class MilksComponent implements OnInit{
-  milks: Milk[] = [
-    {_id: 1, quantity: 134,periodTime:'Tarde',date:'12/04/2022'},
-    {_id: 2, quantity: 114,periodTime:'Tarde',date:'13/04/2022'},
-    {_id: 3, quantity: 124,periodTime:'Tarde',date:'14/04/2022'},
-    {_id: 4, quantity: 104,periodTime:'Tarde',date:'15/04/2022'},
-    {_id: 5, quantity: 164,periodTime:'Tarde',date:'16/04/2022'}
-  ];
+export class MilksComponent implements OnInit {
 
 
-  constructor(private milksService:MilksService) {
+  milks: Observable<Milk[]>;
+  displayedColumns: string[] = ['quantity', 'periodTime', 'date'];
+
+  constructor(private milksService: MilksService) {
+    this.milks = milksService.list()
   }
 
   ngOnInit(): void {
-    this.milksService.findAll();
   }
 
-  displayedColumns: string[] = ['quantity', 'periodTime', 'date'];
+
 }
--- src/app/milks/services/milks.service.ts
+++ src/app/milks/services/milks.service.ts
@@ -1,21 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Milk} from "../model/milk";
 import {HttpClient} from "@angular/common/http";
+import {first, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MilksService {
 
-  constructor(private httpClient:HttpClient) { }
+  private readonly API = '/assets/milks.json'
 
-  findAll():Milk[] {
-    return [
-      {_id: 1, quantity: 134,periodTime:'Tarde',date:'12/04/2022'},
-      {_id: 2, quantity: 114,periodTime:'Tarde',date:'13/04/2022'},
-      {_id: 3, quantity: 124,periodTime:'Tarde',date:'14/04/2022'},
-      {_id: 4, quantity: 104,periodTime:'Tarde',date:'15/04/2022'},
-      {_id: 5, quantity: 164,periodTime:'Tarde',date:'16/04/2022'}
-    ];
+  constructor(private httpClient: HttpClient) {
+  }
+
+  list() {
+    return this.httpClient.get<Milk[]>(this.API).pipe(
+      first(),
+      tap(milks => console.log(milks))
+    );
   }
 }
